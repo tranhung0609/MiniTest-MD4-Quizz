33e5265b60db6354f51145b958aab0c45a3c58c8
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-06-24 14:12:30 +0700

    checkout

---
+++ src/main/java/com/example/minitest2quizzmd4/MiniTest2QuizzMd4Application.java
@@ -0,0 +1,13 @@
+package com.example.minitest2quizzmd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTest2QuizzMd4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniTest2QuizzMd4Application.class, args);
+    }
+
+}
